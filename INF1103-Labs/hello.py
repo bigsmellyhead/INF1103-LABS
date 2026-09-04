265b1f26ba240337d5e241f0e4e5e98ef7614f78
Angeles Josh Luna <2603016@sit.singaporetech.edu.sg>
2026-09-04 15:32:46 +0800

    Follower Tracker

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -7,6 +7,15 @@ username = "angelesjoshluna"
 bio = "handsome"
 followers = 67
 
+followers += 21
+print("Day 1: ", followers)
+
+followers += 69
+print("Day 2: ", followers)
+
+followers -= 81
+print("Day 3: ", followers)
+
 print("Username: ", username)
 print("Bio: ", bio)
 print("Followers: ", followers)
