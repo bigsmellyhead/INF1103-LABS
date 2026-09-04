36ea75fe2c9d1bff11a0ebc2a7f0185299a6406d
Angeles Josh Luna <2603016@sit.singaporetech.edu.sg>
2026-09-04 15:30:39 +0800

    Instagram Thing

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -2,3 +2,11 @@ print("====================")
 print("Welcome here")
 print("My first post! Gila Babi Kontol!!!")
 print("====================")
+
+username = "angelesjoshluna"
+bio = "handsome"
+followers = 67
+
+print("Username: ", username)
+print("Bio: ", bio)
+print("Followers: ", followers)
