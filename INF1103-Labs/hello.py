0d7e302bec798482ce6acae31bad7717378b77ec
Angeles Josh Luna <2603016@sit.singaporetech.edu.sg>
2026-09-04 15:36:38 +0800

    Interactive Profile Creator

--- INF1103-Labs/hello.py
+++ INF1103-Labs/hello.py
@@ -3,19 +3,13 @@ print("Welcome here")
 print("My first post! Gila Babi Kontol!!!")
 print("====================")
 
-username = "angelesjoshluna"
-bio = "handsome"
-followers = 67
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category ")
 
-followers += 21
-print("Day 1: ", followers)
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
 
-followers += 69
-print("Day 2: ", followers)
-
-followers -= 81
-print("Day 3: ", followers)
-
-print("Username: ", username)
-print("Bio: ", bio)
-print("Followers: ", followers)
